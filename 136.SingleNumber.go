67c6171f58b79b237efe033e29640bbe071e431c
masteren <luquanhao@yeah.net>
2020-07-24 16:04:26 +0800

    update 136

---
+++ 136.SingleNumber.go
@@ -0,0 +1,38 @@
+package main
+
+//Given a non-empty array of integers, every element appears twice except for on
+//e. Find that single one.
+//
+// Note:
+//
+// Your algorithm should have a linear runtime complexity. Could you implement i
+//t without using extra memory?
+//
+// Example 1:
+//
+//
+//Input: [2,2,1]
+//Output: 1
+//
+//
+// Example 2:
+//
+//
+//Input: [4,1,2,1,2]
+//Output: 4
+//
+// Related Topics 位运算 哈希表
+// 👍 1383 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func singleNumber(nums []int) int {
+	ans := 0
+	for i := 0; i < len(nums); i++ {
+		ans ^= nums[i]
+	}
+	return ans
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+//2020-07-24 15:53:29
