bc7565abbfb03eed51a69098b1df8e3c5e8f5676
masteren <luquanhao@yeah.net>
2020-07-28 14:00:47 +0800

    feat(Problem): ADD 153, 557

---
+++ 153.FindMinimumInRotatedSortedArray.go
@@ -0,0 +1,42 @@
+package main
+
+//Suppose an array sorted in ascending order is rotated at some pivot unknown to
+// you beforehand.
+//
+// (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+//
+// Find the minimum element.
+//
+// You may assume no duplicate exists in the array.
+//
+// Example 1:
+//
+//
+//Input: [3,4,5,1,2]
+//Output: 1
+//
+//
+// Example 2:
+//
+//
+//Input: [4,5,6,7,0,1,2]
+//Output: 0
+//
+// Related Topics 数组 二分查找
+// 👍 224 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func findMin(nums []int) int {
+	ans := nums[0]
+
+	for _, v := range nums {
+		if ans > v {
+			return v
+		}
+	}
+	return ans
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+//2020-07-28 13:50:34
---
+++ 557.ReverseWordsInAStringIII.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"strings"
+)
+
+//Given a string, you need to reverse the order of characters in each word withi
+//n a sentence while still preserving whitespace and initial word order.
+//
+// Example 1:
+//
+//Input: "Let's take LeetCode contest"
+//Output: "s'teL ekat edoCteeL tsetnoc"
+//
+//
+//
+// Note:
+//In the string, each word is separated by single space and there will not be an
+//y extra space in the string.
+// Related Topics 字符串
+// 👍 205 👎 0
+
+/*
+	func main() {
+		reverseWords("Let's go home.")
+}*/
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func reverseWords(s string) string {
+	ss := strings.Split(s, " ")
+	ans := make([]byte, 0)
+	for _, subs := range ss {
+		for i := len(subs) - 1; i >= 0; i-- {
+			ans = append(ans, subs[i])
+		}
+		ans = append(ans, ' ')
+	}
+	ans = ans[:len(ans)-1]
+	return string(ans)
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+//2020-07-28 13:16:54
