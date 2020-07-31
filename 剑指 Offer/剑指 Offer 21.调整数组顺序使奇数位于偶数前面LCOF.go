e581de00c216d20e4535b6f10dfc0a896667f478
masteren <luquanhao@yeah.net>
2020-07-31 11:56:01 +0800

    feat(剑指 Offer): ADD 剑指 Offer 21

---
+++ 剑指 Offer/剑指 Offer 21.调整数组顺序使奇数位于偶数前面LCOF.go
@@ -0,0 +1,27 @@
+package 剑指_Offer
+
+//English description is not available for the problem. Please switch to Chinese
+//.
+// 👍 33 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func exchange(nums []int) []int {
+	l, r := 0, len(nums)-1
+	for l < r {
+		if nums[l]%2 == 0 && nums[r]%2 == 1 {
+			nums[l], nums[r] = nums[r], nums[l]
+			l++
+			r--
+		} else if nums[r]%2 == 0 {
+			r--
+		} else if nums[l]%2 == 1 {
+			l++
+		}
+	}
+
+	return nums
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+//2020-07-31 11:34:22
