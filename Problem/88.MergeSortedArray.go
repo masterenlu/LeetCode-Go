b7e538c628f39c176df13ce7fb7bc8c1ab70abba
masterenlu <masterenlu@outlook.com>
2021-04-05 18:23:44 +0800

    feat(Problem): add problem.88(easy)

---
+++ Problem/88.MergeSortedArray.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	nums1, nums2 := []int{1, 2, 3, 0, 0, 0}, []int{2, 5, 6}
+	merge(nums1, 3, nums2, 3)
+	fmt.Println(nums1)
+}
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	nums1 = append(nums1[:m], nums2[:n]...)
+	sort.Ints(nums1)
+}
+
+// leetcode submit region end(Prohibit modification and deletion)
+
+// 2021-04-05 16:54:40
+
+// Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one
+// sorted array.
+//
+// The number of elements initialized in nums1 and nums2 are m and n respectivel
+// y. You may assume that nums1 has a size equal to m + n such that it has enough s
+// pace to hold additional elements from nums2.
+//
+//
+// Example 1:
+// Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+// Output: [1,2,2,3,5,6]
+// Example 2:
+// Input: nums1 = [1], m = 1, nums2 = [], n = 0
+// Output: [1]
+//
+//
+// Constraints:
+//
+//
+// nums1.length == m + n
+// nums2.length == n
+// 0 <= m, n <= 200
+// 1 <= m + n <= 200
+// -109 <= nums1[i], nums2[i] <= 109
+//
+//
+// 👍 867 👎 0
